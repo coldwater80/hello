dc0d46adcdd9fb82a307a2cd6f8ca44aafff60bd
coldwater80 <sermetpekin@gmail.com>
2016-03-26 00:19:01 +0200

    asdasd

---
+++ footer/index.php
@@ -0,0 +1,153 @@
+<<!DOCTYPE html>
+<html>
+<head>
+	<title>footer </title>
+	<link rel="stylesheet" href="f.css" type="text/css" />
+</head>
+<body>
+			<div id="footer">
+				<ul id="footer_menu">
+					<li class="home_button"><a href="#"> </a> </li>
+
+					<li><a href="#">Account </a>
+
+					<div class="one_column"> 
+						<div class="col_1"> 
+
+							<a class= "header"> Stream </a>
+							<a class= "header"> Messages </a>
+							<a class= "list_links" style="font-weight: bold; color:white; "> Inbox (5) </a>
+							<a class= "list_links"> Trash  </a>
+							<a class= "list_links"> Compose </a>
+							<a class= "list_links"> Inbox (5) </a>
+
+				
+						</div>
+					</div>
+
+					 </li>
+
+
+		<li><a href="#">About us </a>
+
+					<div class="one_column"> 
+						<div class="col_1"> 
+
+							<a class= "header"> Stream </a>
+							<a class= "header"> Messages </a>
+							<a class= "list_links" style="font-weight: bold; color:white; "> Inbox (5) </a>
+							<a class= "list_links"> Trash  </a>
+							<a class= "list_links"> Compose </a>
+							<a class= "list_links"> Inbox (5) </a>
+
+				
+						</div>
+					</div>
+
+					 </li>
+
+
+
+					 <!-- -->
+
+
+					<li><a href="#">Profile </a> 
+					<div class="three_column"> 
+
+						<div class="col_3"> 
+						<h2> Example example  </h2>
+
+						</div>
+
+
+						<div class="clear"></div> 
+
+
+								<div class="col_1"> 
+								<p> I love tuna so much </p>
+
+								</div>
+									<div class="col_1"> 
+								<p> I love tuna so much </p>
+
+								</div>
+								
+									<div class="col_1"> 
+								<p> I love tuna so much </p>
+
+								</div>
+
+						<div class="clear"></div> 
+
+						<div class="col_1"> 
+								<p> I love tuna so much </p>
+
+								</div>
+						
+						<div class="col_2"> 
+								<p> I love tuna so much </p>
+
+								</div>
+									<div class="clear"></div> 
+
+						<div class="col_1"> 
+								<p> I love tuna so much </p>
+
+								</div>
+						
+						<div class="col_2"> 
+								<p> I love tuna so much </p>
+
+								</div>
+
+
+						
+
+
+					</div>
+					</li>
+
+
+
+					<li><a href="#"> </a> </li>
+					<li><a href="#"> </a> </li>
+
+				</ul>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+				<ul id="notifications">
+				<li> 
+				
+					<a href="#" class="noticons"> <img style="background: transparent;" src ="img/twitter35.png" > 
+
+						<span> xdasdasdadasdfsfvdf
+
+						</span>
+					</a>
+
+				 </li>
+
+				</ul>
+
+
+			</div>
+
+</body>
+</html>
